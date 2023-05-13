eee22f6b7167e1bc3c2feb4ef177336d5b0aa52a
David  Balogh Florex001 <david.balogh.2002@gmail.com>
2023-05-13 15:57:58 +0200

    lekérdezés a dolgozó rekordokhoz

--- SeeGameCeo/src/main/java/hu/seegame/SeeGameCeo/Repositories/WorkshopRepository.java
+++ SeeGameCeo/src/main/java/hu/seegame/SeeGameCeo/Repositories/WorkshopRepository.java
@@ -3,10 +3,14 @@ package hu.seegame.SeeGameCeo.Repositories;
 import hu.seegame.SeeGameCeo.Models.Workshop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkshopRepository extends JpaRepository<Workshop, Integer> {
 
-    Workshop findByMuhelyid(int muhelyid);
+    Workshop findByMuhelyid(int muhelyid);//keresés műhely id alapján
 
-    Workshop findByTulajId(int id);
+    Workshop findByTulajId(int id);//keresés tulajid alapján
+
+    List<Workshop> findByDolgozo1OrDolgozo2OrDolgozo3OrDolgozo4OrDolgozo5OrDolgozo6OrDolgozo7OrDolgozo8OrDolgozo9OrDolgozo10OrDolgozo11OrDolgozo12(String dolgozo1, String dolgozo2, String dolgozo3, String dolgozo4, String dolgozo5, String dolgozo6, String dolgozo7, String dolgozo8, String dolgozo9, String dolgozo10, String dolgozo11, String dolgozo12);//keresi az összes olyan műhelyt amiben dolgozó1 től 12 ig szerepel az ő ic neve
 
 }
